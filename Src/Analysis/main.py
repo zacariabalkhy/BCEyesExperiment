02214bf16cb9a519acf2644df3d2ebabb0940ca9
Zacky <zacaria.balkhy@gmail.com>
2022-10-03 22:27:33 -0700

    plotting fft per trial

--- Src/Analysis/main.py
+++ Src/Analysis/main.py
@@ -4,6 +4,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import json
+from datetime import datetime
+import time
+import math
 
 #Sample Rate = 250 Hz
 sampleRate = 250
@@ -25,7 +28,8 @@ def readDataIntoDF():
             df = pd.DataFrame(data[1:], columns=data[0])
             df.drop(['AccelChannel0', 'AccelChannel1', 'AccelChannel2', 'Other',
         'Other', 'Other', 'Other', 'Other', 'Other', 'Other',
-        'AnalogChannel0', 'AnalogChannel1', 'AnalogChannel2', 'Other'], axis=1, inplace=True)
+        'AnalogChannel0', 'AnalogChannel1', 'AnalogChannel2', 'Other', 'Timestamp(Formatted)'], axis=1, inplace=True)
+            df = df.apply(pd.to_numeric, errors='coerce')
             dataframes.append(df)
     return dataframes
 
@@ -34,8 +38,35 @@ def readTrialData():
     trialdataFileName = "../../Data/AlphaWaveExperimentData/alphaWaveTrialData.json"
     with open(trialdataFileName) as file:
         trialData = json.load(file)
+    return trialData
+
+def splitRawDataIntoTrials(df, trialData):
+    trialSplitData = []
+    rawDataStartTime = df.loc[0]["Timestamp"]
+
     for i in range(len(trialData)):
-        print (trialData[i])
+        trialData[i]["trialNumber"] = i
+        format = "%d-%b-%Y %H:%M:%S.%f"
+
+        startTimeStr = trialData[i]["trialStartTime"]
+        startTime = datetime.strptime(startTimeStr, format)
+        unixStartTime = time.mktime(startTime.timetuple())
+
+        endTimeStr = trialData[i]["trialEndTime"]
+        endTime = datetime.strptime(endTimeStr, format)
+        unixEndTime = time.mktime(endTime.timetuple())
+
+        timeStartDiff = unixStartTime - rawDataStartTime
+        calcSampleStartIndex = math.ceil(timeStartDiff * sampleRate)
+        
+        timeEndDiff = unixEndTime - rawDataStartTime
+        calcSampleEndIndex = math.ceil(timeEndDiff * sampleRate)
+        
+        trialDf = df.iloc[calcSampleStartIndex:calcSampleEndIndex, :]
+        trialData[i]["data"] = trialDf
+        trialSplitData.append(trialData[i])
+
+    return trialSplitData
 
 def createTimeSlices(df, startIndex, length):
     return df[startIndex:startIndex+length][:]
@@ -45,8 +76,8 @@ def createTimeSlices(df, startIndex, length):
 
 def plotsample(df):
 
-    freqAmplitudes = np.abs(np.fft.fft(pd.to_numeric(df[:]["EXGChannel0"])))
-    freqs = np.fft.fftfreq(n=pd.to_numeric(df[:]["EXGChannel0"]).size, d=1/sampleRate)
+    freqAmplitudes = np.abs(np.fft.fft(df[:]["EXGChannel7"]))
+    freqs = np.fft.fftfreq(n=df[:]["EXGChannel7"].size, d=1/sampleRate)
 
     plt.figure()
 
@@ -54,8 +85,8 @@ def plotsample(df):
     plt.stem(freqs[1:], freqAmplitudes[1:], 'b')
     plt.xlabel('Freq (Hz)')
     plt.ylabel('FFT Amplitude |X(freq)|')
-    plt.xlim(0, 250)
-    #plt.ylim(0,100000)
+    plt.xlim(0, 50)
+    plt.ylim(0,10000)
 
     plt.subplot(122)
     plt.plot(pd.to_numeric(df[:]['Timestamp']), pd.to_numeric(df[:]["EXGChannel0"]), "b")
@@ -65,10 +96,13 @@ def plotsample(df):
     plt.show()
 
 if __name__ == "__main__":
-    readTrialData()
+    trialData = readTrialData()
     
     dataframes = readDataIntoDF()
     for dataframe in dataframes:
         print(dataframe.columns)
         print(dataframe.head)
-        plotsample(createTimeSlices(dataframe, 4000, 10000))
+        trials = splitRawDataIntoTrials(dataframe, trialData)
+        for i in range(len(trials)):
+            print(trials[i]["trialType"])
+            plotsample(trials[i]["data"])
